aa492430e0797f53bba8139bc40238cd3cbfe82f
raulrb <rrb22rod@hotmail.com>
2014-09-17 11:22:01 -0600

    cambios en el web.xml y en el servlet

---
+++ src/CO/AgregarColaboradorCo.java
@@ -0,0 +1,26 @@
+package CO;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgregarColaboradorCo extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 826986885787403188L;
+	
+	public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+    throws ServletException, IOException{
+		response.setContentType("text/html");
+		
+		
+	}
+
+	
+}
