847d0c03aabf267188a008c33e25ffa0ca9779c5
raulrb <rrb22rod@hotmail.com>
2014-09-17 09:20:32 -0600

    primer envio

---
+++ WebContent/AgrearColaborador.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="CSS/CSS.css">
+<title>AgregarColaborador</title>
+</head>
+<body>
+
+<div class="header">Agregar un nuevo colaborador</div>
+<div class="menu">
+<a href=".">Agregar nuevo colaborador</a><br/>
+<a href=".">Ver lista de colaboradores</a>
+</div>
+<div class="cuerpo" id="cuerpo">
+<form>
+Nombre: <input type="text"/>
+
+Apellidos: <input type="text"/>
+
+Cédula: <input type="text"/>
+
+Correo: <input type="text"/>
+Teléfono de casa: <input type="text"/>
+Teléfono de celular: <input type="text"/>
+
+Nivel de Inglés: <input type="text"/>
+
+Dirección: <input type="text"/>
+Observaciones: <input type="text"/>
+</form>
+</div>
+</body>
+</html>
---
+++ src/BO/ColaboradorBo.java
@@ -0,0 +1,69 @@
+package BO;
+
+public class ColaboradorBo {
+
+	private String nombre;
+	private String apellidos;
+	private int cedula;
+	private int telefonoCasa;
+	private int telefonoCelular;
+	private String correo;
+	private String direccion;
+	private int nivelIngles;
+	private String observaciones;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getCedula() {
+		return cedula;
+	}
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	public int getTelefonoCasa() {
+		return telefonoCasa;
+	}
+	public void setTelefonoCasa(int telefonoCasa) {
+		this.telefonoCasa = telefonoCasa;
+	}
+	public int getTelefonoCelular() {
+		return telefonoCelular;
+	}
+	public void setTelefonoCelular(int telefonoCelular) {
+		this.telefonoCelular = telefonoCelular;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getNivelIngles() {
+		return nivelIngles;
+	}
+	public void setNivelIngles(int nivelIngles) {
+		this.nivelIngles = nivelIngles;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+	
+}
