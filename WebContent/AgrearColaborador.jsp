72573135b7548fa49bfd07393693ba23959125e7
pablo <pablobar23@gmail.com>
2014-09-18 11:23:36 -0600

    js y CSS del formColaborador

--- WebContent/AgrearColaborador.jsp
+++ WebContent/AgrearColaborador.jsp
@@ -3,32 +3,34 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" type="text/css" href="CSS/CSS.css">
-<title>AgregarColaborador</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="CSS/CSS.css">
+	<link rel="stylesheet" type="text/css" href="CSS/FormStyle.css">
+	<title>AgregarColaborador</title>
 </head>
 <body>
 
-<div class="header">Agregar un nuevo colaborador</div>
-<div class="menu">
-<a href="AgregarColaborador.jsp">Agregar nuevo colaborador</a><br/>
-<a href="ListarColaboradores.jsp">Ver lista de colaboradores</a>
-</div>
-<div class="cuerpo" id="cuerpo">
-<form name="opcionMenu" method="post" id="opcionMenu" action="AgregarColaboradorCo" onsubmit="return validar(this)">
-<ul>
-<li>Nombre: <input type='text' name= "txtNombre" id='txtNombre'/><span id="errorNombre" class="errorStyle"></span></li>
-<li>Apellidos: <input type='text' name= "txtApellidos" id='txtApellidos'/><span id="errorApellidos" class="errorStyle"></span></li>
-<li>Cédula:<input type='text' name= "txtCedula" id='txtCedula'/><span id="errorCedula" class="errorStyle"></span></li>
-<li>Correo electrónico: <input type='text' name= "txtEmail" id='txtEmail'/><span id="errorEmail" class="errorStyle"></span></li>
-<li>Teléfono de casa: <input type='text' name= "txtTelCasa" id='txtTelCasa'/><span id="errorTelCasa" class="errorStyle"></span></li>
-<li>Teléfono de celular: <input type='text' name= "txtTelCel" id='txtTelCel'/><span id="errorTelCel" class="errorStyle"></span></li>
-<li>Nivel de Inglés: <input type='text' name= "txtNvlIngles" id='txtNvlIngles'/><span id="errorNvlIngles" class="errorStyle"></span></li>
-<li>Dirección: <input type='text' name= "txtDireccion" id='txtDireccion'/><span id="errorDireccion" class="errorStyle"></span></li>
-<li>Observaciones: <input type='text' name= "txtObservaciones" id='txtObservaciones'/><span id="errorObservaciones" class="errorStyle"></span></li>
-</ul>
-<input name="btnAgregar" type="submit" id="btnAgregar" value="Agregar" >
-</form>
-</div>
+	<div class="header">Agregar un nuevo colaborador</div>
+	<div class="menu">
+		<a href="AgregarColaborador.jsp">Agregar nuevo colaborador</a><br/>
+		<a href="ListarColaboradores.jsp">Ver lista de colaboradores</a>
+	</div>
+	<div class="cuerpo" id="cuerpo">
+		<form name="opcionMenu" method="post" id="opcionMenu" action="AgregarColaboradorCo" onsubmit="return validarForm(this)">
+			<ul>
+				<li>Nombre: <input type='text' id='txtNombre'/><span id="errorNombre" class="errorStyle"></span></li>
+				<li>Apellidos: <input type='text' id='txtApellidos'/><span id="errorApellidos" class="errorStyle"></span></li>
+				<li>Cédula:<input type='text'id='txtCedula'/><span id="errorCedula" class="errorStyle"></span></li>
+				<li>Correo electrónico: <input type='text' id='txtEmail'/><span id="errorEmail" class="errorStyle"></span></li>
+				<li>Teléfono de casa: <input type='text' id='txtTelCasa'/><span id="errorTelCasa" class="errorStyle"></span></li>
+				<li>Teléfono de celular: <input type='text' id='txtTelCel'/><span id="errorTelCel" class="errorStyle"></span></li>
+				<li>Nivel de Inglés: <input type='text' id='txtNvlIngles'/><span id="errorNvlIngles" class="errorStyle"></span></li>
+				<li>Dirección: <input type='text' id='txtDireccion'/><span id="errorDireccion" class="errorStyle"></span></li>
+				<li>Observaciones: <input type='text' id='txtObservaciones'/><span id="errorObservaciones" class="errorStyle"></span></li>
+			</ul>
+		<input name="btnAgregar" type="submit" id="btnAgregar" value="Agregar" >
+		</form>
+	</div>
+	<script type="text/javascript" src="JS/validarFormColaborador.js"></script>
 </body>
 </html>
---
+++ WebContent/JS/validarFormColaborador.js
@@ -0,0 +1,162 @@
+	//Función para validar campos de un formulario.
+	document.getElementById('btnAgregar').onclick=function validarForm () {
+
+		var nombre, apellidos, cedula, email, telCasa, telCel, nvlIngles, datosValidos;		
+		
+		//Almacenamiento de cada value del formulario en una variable .	
+		nombre = document.getElementById('txtNombre').value;
+		apellidos = document.getElementById('txtApellidos').value;
+	cedula = document.getElementById('txtCedula').value;
+	email = document.getElementById('txtEmail').value;
+	telCasa = document.getElementById('txtTelCasa').value;
+	telCel = document.getElementById('txtTelCel').value;
+	nvlIngles = document.getElementById('txtNvlIngles').value;
+      	
+      	// Validar nombre.
+		if (estaVacio(nombre, 'errorNombre', 'txtNombre')){
+			datosValidos=false;			
+		}else{
+			if(esTexto(nombre, 'errorNombre', 'txtNombre')){
+				datosValidos=true;
+			}	
+		}
+
+		//Validar apellidos.
+		if (estaVacio(apellidos, 'errorApellidos', 'txtApellidos')){
+			datosValidos=false;	
+		}else{
+			if(esTexto(apellidos, 'errorApellidos', 'txtApellidos')){
+				datosValidos=true;
+			}
+		}
+
+		//Validar cedula.
+		if (estaVacio(cedula, 'errorCedula', 'txtCedula')){
+			datosValidos=false;	
+		}else{
+			if(esNumero(cedula, 'errorCedula', 'txtCedula')){
+				datosValidos=true;
+			}
+		}	
+
+		//Validar email.
+		if (estaVacio(email, 'errorEmail', 'txtEmail')){
+			datosValidos=false;	
+		}else{
+			if(validarEmail(email, 'errorEmail', 'txtEmail')){
+				datosValidos=true;
+			}
+		}	
+
+		//Validar telefono de casa. 
+		if (esNumero(telCasa, 'errorTelCasa', 'txtTelCasa')){
+			datosValidos=true;	
+		}else{
+			datosValidos=false;
+		}	
+
+		//Validar telefono celular.
+		if(esNumero(telCel, 'errorTelCel', 'txtTelCel')){
+			datosValidos=true;
+		}else{
+			datosValidos=false;
+		}	
+
+		//Validar nivel de ingles.
+		if(esNumero(nvlIngles, 'errorNvlIngles', 'txtNvlIngles')){
+			datosValidos=true;
+		}else{
+			datosValidos=false;
+		}
+
+  		/*Función para validar que el campo no esté vacío. 
+  		Parametros: pdato, valor a evaluar.
+  					pspan, nombre del span donde se desplegará el error.
+  					pcampo, campo del formulario que será demarcado en campo de que tenga algún error.
+		Retorna:    esValido, verifica si es valido o no.
+  		*/
+        function estaVacio(pdato, pspan, pcampo)  {
+
+          	var estaVacio = false;
+
+          	if (pdato === ''){
+          		document.getElementById(pcampo).className='error';
+				document.getElementById(pspan).innerHTML='* Información requerida ';	
+				estaVacio = true;
+          	} else {
+          		document.getElementById(pcampo).className='limpiarInput';
+          		document.getElementById(pspan).innerHTML='';	
+          	}
+          	return estaVacio;
+          }
+
+   		/*Función para validar que el campo sea numérico. 
+  		Parametros: pdato, valor a evaluar.
+  					pspan, nombre del span donde se desplegará el error.
+  					pcampo, campo del formulario que será demarcado en campo de que tenga algún error.
+		Retorna:    esValido, verifica si es valido o no.
+  		*/
+        function esNumero(pdato, pspan, pcampo)  {
+
+          	var esValido = true,
+          	regexNum = /^[0-9]*$/;
+
+          	if (regexNum.test(pdato) == false){
+          		document.getElementById(pcampo).className='errorInput';
+				document.getElementById(pspan).innerHTML='* Formato inválido';	
+				esValido = false;
+          	} else {
+          		document.getElementById(pcampo).className='limpiarInput';
+          		document.getElementById(pspan).innerHTML='';	
+          	}
+          	return esValido;
+          }
+
+      	/*Función para validar que el campo sea texto. 
+		Parametros: pdato, valor a evaluar.
+					pspan, nombre del span donde se desplegará el error.
+					pcampo, campo del formulario que será demarcado en campo de que tenga algún error
+		Retorna:    esValido, verifica si es valido o no.
+		*/
+      	function esTexto(pdato, pspan, pcampo)  {
+
+      	var esValido = true,
+      	regexChar = /^[A-Za-z\s'. áéíóúÁÉÍÓÚÑñÜü]+$/;
+
+      	if (regexChar.test(pdato) == false){
+      		document.getElementById(pcampo).className='error';
+			document.getElementById(pspan).innerHTML='* Formato inválido';	
+			esValido = false;
+      	} else {
+      		document.getElementById(pcampo).className='limpiarInput';
+      		document.getElementById(pspan).innerHTML='';	
+      	}
+          	return esValido;
+        }
+
+        /*Función para validar que el campo contenga un email con formato valido. 
+		Parametros: pdato, valor a evaluar.
+					pspan, nombre del span donde se desplegará el error.
+					pcampo, campo del formulario que será demarcado en campo de que tenga algún error.
+		Retorna:    esValido, verifica si es valido o no.
+		*/
+     	function validarEmail(dato, span, campo)  {
+
+			var esValido = true,
+	      	regexCorreo = /^[0-9a-z_\-\.]+@[0-9a-z\-\.]+\.[a-z]{2,4}$/i;
+
+	      	if (regexCorreo.test(dato) == false){
+	      		document.getElementById(campo).className='error';
+				document.getElementById(span).innerHTML='* Formato inválido';	
+				esValido = false;
+	      	} else {
+	      		document.getElementById(campo).className='limpiarInput';
+	      		document.getElementById(span).innerHTML='';	
+	      	}
+	      	return esValido;
+	    }
+     	
+     	return datosValidos;
+   };
+
+   
