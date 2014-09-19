ecec0c06374d1508f20abddd1b024d4314c9cbcd
pablo <pablobar23@gmail.com>
2014-09-19 08:41:48 -0600

    fix al al js

--- WebContent/JS/validarFormColaborador.js
+++ WebContent/JS/validarFormColaborador.js
@@ -101,8 +101,8 @@
         function esNumero(pdato, pspan, pcampo)  {
 
           	var esValido = true,
-<<<<<<< HEAD
           	regexNum = /^\d+/;
+          	
           	if(pdato !== ""){
           		if (regexNum.test(pdato) == false){
               		document.getElementById(pcampo).className='errorInput';
@@ -113,18 +113,6 @@
               		document.getElementById(pspan).innerHTML='';	
               	}
           	}          	
-=======
-          	regexNum = /^[0-9]*$/;
-
-          	if (regexNum.test(pdato) === false){
-          		document.getElementById(pcampo).className='errorInput';
-				document.getElementById(pspan).innerHTML='* Formato inválido';	
-				esValido = false;
-          	} else {
-          		document.getElementById(pcampo).className='limpiarInput';
-          		document.getElementById(pspan).innerHTML='';	
-          	}
->>>>>>> branch 'master' of https://github.com/raulrb/IMECColaboradores.git
           	return esValido;
           }
 
